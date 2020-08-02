36124b5758f9d98c723cccdd8c91a8aeeee8662b
Ivan Banovacki <banovacki.ivan@gmail.com>
2020-08-02 15:42:53 +0200

    Uradjen domaci za nedelju

---
+++ Nedelja3/Nedelja/zadatak2.js
@@ -0,0 +1,15 @@
+// Написати функцију која враћа колико пице вам је довољно за доживотно снабдевање
+// Уноси се колико пице поједете на месечном нивоу, и колико година имате (Рачуна се да је довољно достављати до стоте године)
+
+
+// lifeSupply(10,26) // Враћа 8880
+// console.log(lifeSupply(10,26)) // Исписује 8880
+
+
+function lifeSupply(numPerMonth,age){
+    if(numPerMonth <= 0 || age < 0){
+        return 'Unesite validne parametre'
+    }
+    else return (100 - age) * 12 * numPerMonth
+}
+console.log(lifeSupply(10,26));
